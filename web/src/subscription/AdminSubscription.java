f60f7abafc8abb263e0419e0edca87b7be723496
Chitender Kumar <chitenderkumar.16@gmail.com>
2016-11-23 14:24:42 +0530

    c

--- web/src/subscription/AdminSubscription.java
+++ web/src/subscription/AdminSubscription.java
@@ -1,31 +1,39 @@
 package subscription;
 import web.Form;
+import web.Form.thehandler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class AdminSubscription{
-	Form form = new Form();
-	private final String USER_AGENT = "Mozilla/5.0";
-private final String PAID_URL = ("http://127.0.0.1:11088/testpfsapi/test?cmd=registrationRequest&1=91" + form.tf1 + "&2=true&3=" + form.tf6);
-private final String DEMO_URL = ("http://127.0.0.1:11088/testpfsapi/test?cmd=registrationCommit&1=91" + form.tf1 + "&2=true&3=" + form.tf6);
 
+
+
+public class AdminSubscription{
+	
+	private final String USER_AGENT = "Mozilla/5.0";
+		
+	Form form = new Form();
+		public String admin = form.tf1.getText();
+		public String campaign = form.tf6.getText();
+		
+		
+private final String URL = ("http://127.0.0.1:11088/testpfsapi/test?cmd=registrationRequest&1=91" +admin + "&2=true&3=" +campaign);
 
 
 public void activation() throws IOException {
-	PaidActivation();
-	System.out.println("Activation DONE");
-	DemoActivation();
-	System.out.println("Demo Registration DONE");
-}
+		PaidActivation();
+		System.out.println(URL);
+		System.out.println("Activation DONE");
+	
+	 }
+
 
 private void PaidActivation() throws IOException {
-	URL obj = new URL(PAID_URL);
+	URL obj = new URL(URL);
 	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 	con.setRequestMethod("GET");
 	con.setRequestProperty("User-Agent", USER_AGENT);
@@ -50,30 +58,5 @@ private void PaidActivation() throws IOException {
 
 }
 
-private void DemoActivation() throws IOException {
-	URL obj = new URL(DEMO_URL);
-	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	con.setRequestMethod("GET");
-	con.setRequestProperty("User-Agent", USER_AGENT);
-	int responseCode = con.getResponseCode();
-	System.out.println("GET Response Code :: " + responseCode);
-	if (responseCode == HttpURLConnection.HTTP_OK) { // success
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		// print result
-		System.out.println(response.toString());
-	} else {
-		System.out.println("GET request not worked");
-
-}
-}
 }
 
--- web/src/web/Form.java
+++ web/src/web/Form.java
@@ -1,46 +1,48 @@
 package web;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
 import subscription.AdminSubscription;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
 public class Form extends JFrame {
-	public JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10;
-	public JTextField tf1, tf2, tf3, tf4, tf5, tf6, tf7, tf8, tf9;
-	public JButton b1, b2;
+	public static JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10;
+	public static JTextField tf1, tf2, tf3, tf4, tf5, tf6, tf7, tf8, tf9;
+	public static JButton b1, b2;
 	public Form(){
 		
 		super("ENTER ADMIN DETAILS");
 		
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		l1 = new JLabel("ENTER ADMIN DETAILS", JLabel.CENTER);
+		l1 = new JLabel("ENTER ADMIN DETAILS", SwingConstants.CENTER);
 		l1.setForeground(Color.blue);
         l1.setFont(new Font("Serif", Font.BOLD, 20));
-		l2 = new JLabel("Admin MSISDN", JLabel.LEFT);
+		l2 = new JLabel("Admin MSISDN", SwingConstants.LEFT);
 		tf1 = new JTextField(10);
-		l3 = new JLabel("Admin Name",JLabel.LEFT);
+		l3 = new JLabel("Admin Name",SwingConstants.LEFT);
 		tf2 = new JTextField(10);
-		l4 = new JLabel("Company Name",JLabel.LEFT);
+		l4 = new JLabel("Company Name",SwingConstants.LEFT);
 		tf3 = new JTextField(10);
-		l5 = new JLabel("COCP/COIP",JLabel.LEFT);
+		l5 = new JLabel("COCP/COIP",SwingConstants.LEFT);
 		tf4 = new JTextField(10);
-		l6 = new JLabel("Circle Code",JLabel.LEFT);
+		l6 = new JLabel("Circle Code",SwingConstants.LEFT);
 		tf5 = new JTextField(10);
-		l7 = new JLabel("Campaign Id",JLabel.LEFT);
+		l7 = new JLabel("Campaign Id",SwingConstants.LEFT);
 		tf6 = new JTextField(10);
-		l8 = new JLabel("Email ID",JLabel.LEFT);
+		l8 = new JLabel("Email ID",SwingConstants.LEFT);
 		tf7 = new JTextField(10);
-		l9 = new JLabel("Agent Code",JLabel.LEFT);
+		l9 = new JLabel("Agent Code",SwingConstants.LEFT);
 		tf8 = new JTextField(10);
-		l10 = new JLabel("Sub Agent Code",JLabel.LEFT);
+		l10 = new JLabel("Sub Agent Code",SwingConstants.LEFT);
 		tf9 = new JTextField(10);		
 		b1 = new JButton("SUBMIT");
 		b2 = new JButton("CLEAR");
@@ -103,14 +105,16 @@ public class Form extends JFrame {
 		
 	}
 	
-	private class thehandler implements ActionListener{
+	public static class thehandler implements ActionListener{
 		
 	
-    	public void actionPerformed(ActionEvent event) 
+    	@Override
+		public void actionPerformed(ActionEvent event) 
      {
         if (event.getSource() == b1)
          {
-            String s1 = tf1.getText();
+        	
+        	String s1 = tf1.getText();
             String s2 = tf2.getText();
             String s3 = tf3.getText();
             String s4 = tf4.getText();
@@ -120,9 +124,18 @@ public class Form extends JFrame {
             String s8 = tf8.getText();
             String s9 = tf9.getText();
             
-            new AdminSubscription();
-                        
+            JOptionPane.showMessageDialog(null, "Admin MSISDn is" +s1);
+            JOptionPane.showMessageDialog(null, "CAMPAIGN ID is" +s6);
+            AdminSubscription paidactivation = new AdminSubscription();
+            try {
+            	
+				paidactivation.activation();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
             
+        	            
             
          }
 }
